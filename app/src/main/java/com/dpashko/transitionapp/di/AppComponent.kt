f599656ce9f9a2f1984b19b9182fadd3281db238
Dmytro-Pashko <pashko.dmytro@gmail.com>
2019-11-22 15:46:50 +0200

    Removed Dagger support.

--- app/src/main/java/com/dpashko/transitionapp/di/AppComponent.kt
+++ app/src/main/java/com/dpashko/transitionapp/di/AppComponent.kt
@@ -1,16 +1,11 @@
 package com.dpashko.transitionapp.di
 
 import android.app.Application
-
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
-
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.AndroidSupportInjectionModule
-
 import javax.inject.Singleton
 
 @Singleton
