de79085b345b0a84a517f07f395a98fdedf6ce97
Dmytro Pashko <pashko.dmytro@gmail.com>
2019-11-23 21:52:07 +0200

    Implemented proguard rules.

--- app/src/main/java/com/dpashko/transitionapp/ui/CountriesListActivity.kt
+++ app/src/main/java/com/dpashko/transitionapp/ui/CountriesListActivity.kt
@@ -1,6 +1,7 @@
 package com.dpashko.transitionapp.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
